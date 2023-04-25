e09e87845e02f37071d858fef7bd32e44f8a05a7
Yifan Bian <bianya@mail.uc.edu>
2023-04-25 16:46:23 -0400

    Migration

---
+++ Movies/Migrations/20230425204548_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Movies.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserModels",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SessionId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserModels", x => x.UserId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserModels",
+                columns: new[] { "UserId", "Email", "Password", "SessionId", "Username" },
+                values: new object[] { 1, "bianya@mail.uc.edu", "12345", null, "yifan" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserModels");
+        }
+    }
+}
