d6a2ef0c475804a001c586622510d0b9d6baf764
Yifan Bian <bianya@mail.uc.edu>
2023-04-25 16:47:22 -0400

    usermodel and Dbcontext

---
+++ Movies/Models/MovieDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Movies.Models
+{
+	public class MovieDbContext : DbContext
+	{
+		public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<UserModel> UserModels { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<UserModel>(entity =>
+			{
+				entity.HasData(new UserModel { UserId = 1, Username = "yifan", Password = "12345", Email = "bianya@mail.uc.edu", ConfirmPassword = "12345" });
+
+			});
+		}
+	}
+}
---
+++ Movies/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Movies.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net.Http;
+
+namespace Movies.Models
+{
+	public class UserModel
+	{
+		[Key]
+		public int UserId { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string Username { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[NotMapped]
+		[Required]
+		[Compare("Password")]
+		[DataType(DataType.Password)]
+		public string ConfirmPassword { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string Email { get; set; }
+
+		public string SessionId { get; set; }
+	}
+}
