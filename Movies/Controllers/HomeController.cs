0340d2ce4b4ed89469d5819421e02daf41308e50
Yifan Bian <bianya@mail.uc.edu>
2023-04-25 16:50:06 -0400

    layout and controller

--- Movies/Controllers/HomeController.cs
+++ Movies/Controllers/HomeController.cs
@@ -1,22 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Movies.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Movies.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+		private readonly MovieDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, MovieDbContext context)
         {
             _logger = logger;
-        }
+			_context = context;
+		}
 
         public IActionResult Index()
         {
@@ -33,10 +33,80 @@ namespace Movies.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		public ActionResult Register()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+
+		[HttpPost]
+		public IActionResult Login(UserModel model)
+		{
+			var user = _context.UserModels.FirstOrDefault(u => u.Username == model.Username && u.Password == model.Password);
+			if (user != null)
+			{
+				user.SessionId = HttpContext.Session.Id;
+				_context.SaveChanges();
+
+				HttpContext.Session.SetInt32("UserId", user.UserId);
+				HttpContext.Session.SetString("SessionId", user.SessionId);
+				HttpContext.Session.SetString("Username", user.Username);
+
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Invalid username or password.");
+				return View(model);
+			}
+		}
+
+		[HttpPost]
+		public IActionResult Logout()
+		{
+			HttpContext.Session.Clear();
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpPost]
+		public IActionResult Register(UserModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var existingUser = _context.UserModels.FirstOrDefault(u => u.Username == model.Username);
+				if (existingUser != null)
+				{
+					ModelState.AddModelError("Username", "Username already exists.");
+				}
+				else
+				{
+					var user = new UserModel
+					{
+						Username = model.Username,
+						Password = model.Password,
+						ConfirmPassword = model.ConfirmPassword,
+						Email = model.Email
+					};
+
+					_context.UserModels.Add(user);
+					_context.SaveChanges();
+
+					return RedirectToAction("Login", "Home");
+				}
+			}
+
+			return View(model);
+		}
+
+	}
 }
--- Movies/Startup.cs
+++ Movies/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Movies.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,10 +26,19 @@ namespace Movies
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+			services.AddSession();
+
+			services.AddDbContext<MovieDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("MovieDbContext")));
+
+			services.AddHttpContextAccessor();
+
+
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,7 +57,9 @@ namespace Movies
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+			app.UseSession();
+
+			app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
