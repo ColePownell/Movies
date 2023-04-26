e8c10b1f1652fedf75b7c8b8716f2bc34e2c77b7
Yifan Bian <bianya@mail.uc.edu>
2023-04-26 06:46:53 -0400

    add movie model

---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies.Models
+{
+	public class Movie
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Poster { get; set; }
+
+		[NotMapped]
+		public IFormFile PosterFile { get; set; }
+	}
+}
